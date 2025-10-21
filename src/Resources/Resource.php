cd64bbcc4da118db22f9fd16f8171412f2ebb37a
MC0RE <michael@mcore-services.be>
2025-10-21 16:00:39 +0200

    Fix: Accept all UUID formats

--- src/Resources/Resource.php
+++ src/Resources/Resource.php
@@ -318,7 +318,7 @@ abstract class Resource
      */
     protected function validateId(string $id): void
     {
-        if (! preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i', $id)) {
+        if (! preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i', $id)) {
             throw new InvalidArgumentException("Invalid UUID format: {$id}");
         }
     }
